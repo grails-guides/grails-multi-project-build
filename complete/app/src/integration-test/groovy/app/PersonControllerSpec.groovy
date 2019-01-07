a41e2ce8f7ea4263e2bee8d3634f674b99bfef40
sdelamo <sergio.delamo@softamo.com>
2019-01-07 12:13:55 +0100

    Use micronaut HTTP Client dependency

--- complete/app/src/integration-test/groovy/app/PersonControllerSpec.groovy
+++ complete/app/src/integration-test/groovy/app/PersonControllerSpec.groovy
@@ -2,21 +2,32 @@ package app
 
 import demo.OCI
 import grails.testing.mixin.integration.Integration
+import grails.testing.spock.OnceBefore
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.client.HttpClient
+import spock.lang.AutoCleanup
+import spock.lang.Shared
 import spock.lang.Specification
-import grails.plugins.rest.client.RestBuilder
 
 @Integration
 class PersonControllerSpec extends Specification {
 
+    @Shared
+    @AutoCleanup
+    HttpClient client
+
+    @OnceBefore
+    void init() {
+        final String baseUrl = "http://localhost:$serverPort"
+        this.client = HttpClient.create(new URL(baseUrl))
+    }
+
     def '/person endpoints return one of the names'() {
-
-        given:
-        RestBuilder rest = new RestBuilder()
-
         when:
-        def resp = rest.get("http://localhost:${serverPort}/person")
+        HttpResponse<String> resp = client.toBlocking().exchange(HttpRequest.GET("/person"), String)
 
         then:
-        OCI.PEOPLE.contains resp.text
+        OCI.PEOPLE.contains resp.body()
     }
 }
